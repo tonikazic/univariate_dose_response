8131606d7ab4b023de8de89aa9caa8e607d9c0ab
Toni Kazic <kazict@missouri.edu>
2019-02-11 08:35:49 -0600

    added a sample file of input parameters, 11.2.2019
    When a sweep of simulations is started, this file is read first to establish the local grid of parameter values for the simulations.

---
+++ parm50.r
@@ -0,0 +1,17 @@
+# standard evaluation interval for :ann:'s data
+
+x=seq(-42,42,0.5) ;  y=seq(-7.5,7.5,0.5)
+
+
+# refinement for more domed surfaces, especially B73.
+#
+# Stapleton and Kazic, 16.10.2016
+
+# 320,320 steps
+
+
+a <- seq(0.001,0.02,0.002)
+b <- seq(0.18,0.3,0.02)
+c <- c(seq(-3,-1.8,0.1))
+d <- seq(0.4,2.5,0.1)
+e <- seq(4.5,6,0.1)
